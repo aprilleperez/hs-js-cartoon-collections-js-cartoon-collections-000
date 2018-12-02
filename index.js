50ac74e4b4c087fb3b0e860e696c880489520d30
Aprille P <aprillesakura@gmail.com>
2018-12-02 08:36:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var string = [];
+	dwarves.forEach(function(name, i) {
+		string.push(i+1 +". " + name + " ");
+	});
+	return string.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = [];
+    planeteerCalls.forEach(function(ele) {
+  	ele = ele.toUpperCase() + "!";
+  	calls.push(ele);
+  });
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ["camembert", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+		var cheeseIdx = cheese.indexOf(foods[i]);
+		if(cheeseIdx !== -1) {
+			return foods[i];
+		}
+	}
+	return "no cheese!";
 }
